b3a52d4c3004123ba80d3128515c17984c0cdb38
emanuele.aliberti <emanuele.aliberti@mtka.eu>
2023-01-08 16:16:49 +0100

    fix layout add minimal CSS

--- src/system-disk-stats-report.js
+++ src/system-disk-stats-report.js
@@ -31,7 +31,7 @@ const program = new Command();
 program
     .name('system-disk-stats-report')
     .description('CLI command to send a report on local disks.')
-    .version('1.0.0')
+    .version('1.0.1')
     .requiredOption('--from <address>', 'the sender of the e-mail message')
     .requiredOption('--to <address>', 'the recipient of the e-mail message')
     .option('--subject <text>', 'the subject of the e-mail message', 'Disk Statistics');
@@ -43,12 +43,38 @@ const reportTemplate = `<!DOCTYPE html>
 <html lang="en">
   <head>
     <title>{{subject}}</title>
+    <style>
+    body {
+      font-family: sans-serif;
+      font-size: 12pt;
+    }
+    table {
+      border-collapse: collapse;
+      border: 1px solid black;
+      text-align: center;
+      vertical-align: middle;
+    }
+    caption {
+      font-weight: bold;
+      font-size: 20pt;
+      text-align: left;
+      color: #333;
+      margin-bottom: 8px;
+    }
+    th, td {
+      border: 1px solid black;
+      padding: 4px;
+    }
+    td.counter {
+        text-align: right;
+    }
+    </style>
   </head>
   <body>
-    <h1>{{subject}}</h1>
-    <p>MDaemon {{mdInfo.version.full}} runs on <strong>{{osInfo.hostname}}</strong>
-    {{osInfo.distro}} {{osInfo.release}} {{osInfo.arch}} (build {{osInfo.build}}).</p>
+    <p>MDaemon {{mdInfo.version.full}} runs on <strong>{{osInfo.hostname}}</strong>.</p>
+    <p><strong>{{osInfo.hostname}}</strong> runs {{osInfo.distro}} {{osInfo.release}} {{osInfo.arch}} (build {{osInfo.build}}).</p>
     <table>
+      <caption>{{subject}}</caption>
       <thead>
         <th>#</th>
         <th>File System</th>
@@ -58,6 +84,7 @@ const reportTemplate = `<!DOCTYPE html>
         <th>Available</th>
         <th>Used</th>
         <th>Mount</th>
+        <th>R/W</th>
       </thead>
       <tbody>
         {{#each fsSize}}
@@ -66,18 +93,18 @@ const reportTemplate = `<!DOCTYPE html>
             {{#with this}}
             <td>{{fs}}</td>
             <td>{{type}}</td>
-            <td>{{size}}</td>
-            <td>{{used}}</td>
-            <td>{{available}}</td>
-            <td>{{use}}%</td>
+            <td class="counter">{{size}}</td>
+            <td class="counter">{{used}}</td>
+            <td class="counter">{{available}}</td>
+            <td class="counter">{{use}}%</td>
             <td>{{mount}}</td>
             <td>{{#if mount}}&#9745;{{else}}{{/if}}</td>
             {{/with}}
         </tr>
         {{/each}}
       </tbody>
-      <tfoot><small>Generated on {{timestamp}}</small></tfoot>
     </table>
+    <p><small><small>Generated at {{timestamp}}.</small></small></p>
   </body>
 </html>`;
 
